b0b3d45d68d64e94315d3d485bddb507623eec4f
Júnior de Oliveira <ellismar.oliveira@syngenta.com>
2023-06-30 07:25:23 -0300

    feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { Header } from './components/header/header';
+import './styles/global.css';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <Header />;
 }
 
 export default App;
