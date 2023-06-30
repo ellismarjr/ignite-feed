af4943db09a9b9d653e5b8df1d3d3069df382e17
Júnior de Oliveira <ellismar.oliveira@syngenta.com>
2023-06-30 08:00:22 -0300

    feat: profile component

---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,31 @@
+import { PencilLine } from 'phosphor-react';
+import styles from './sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=40"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/ellismarjr.png"
+          alt=""
+        />
+        <strong>Júnior de Oliveira</strong>
+        <span>Frontend Engineer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
