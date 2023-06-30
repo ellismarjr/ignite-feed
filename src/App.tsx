b75d6e4f201fed2372bad7c45bd7030336fb7e40
Júnior de Oliveira <ellismar.oliveira@syngenta.com>
2023-06-30 08:01:17 -0300

    feat: show sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import { Header } from './components/header/header';
 import './styles/global.css';
+import styles from './App.module.css';
+
+import { Sidebar } from './components/sidebar/sidebar';
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+      </div>
+    </div>
+  );
 }
 
 export default App;
