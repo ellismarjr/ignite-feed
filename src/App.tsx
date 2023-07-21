40e2f0b49e956fb252a396d7ea1fefedff59a5c2
Júnior de Oliveira <ellismar.oliveira@syngenta.com>
2023-07-21 15:13:19 -0300

    feat: add comment component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Header } from './components/header/header';
 import './styles/global.css';
 import styles from './App.module.css';
-
+import { Post } from './components/post/post';
 import { Sidebar } from './components/sidebar/sidebar';
 
 function App() {
@@ -13,6 +13,10 @@ function App() {
         <aside>
           <Sidebar />
         </aside>
+        <main>
+          <Post />
+          <Post />
+        </main>
       </div>
     </div>
   );
---
+++ src/components/comment/Comment.tsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/ellismarjr.png" alt="Avatar" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Júnior de Oliveira</strong>
+              <time title="11 de maio às 08:17" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom dev. Parabéns.</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
